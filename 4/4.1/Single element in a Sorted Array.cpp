87359a0f7850ca6567e5e1b0f9b701ac516abd6a
Aryan Abraham <abraham.aryan01@gmail.com>
2023-04-29 08:58:21 +0530

    Update Single element in a Sorted Array.cpp

--- 4/4.1/Single element in a Sorted Array.cpp
+++ 4/4.1/Single element in a Sorted Array.cpp
@@ -4,17 +4,22 @@ public:
         int n=nums.size();
         int low=0, high=n-1;
 
-        if(high==0) return nums[0];
-        else if(nums[low]!=nums[1]) return nums[low];
-        else if(nums[high-1]!=nums[high]) return nums[high];
+        if(n<2||nums[low]!=nums[low+1]) return nums[low];
+        if(n>2&&nums[high]!=nums[high-1]) return nums[high];
 
         while(low<=high){
             int mid=low+(high-low)/2;
 
-            if(nums[mid-1]!=nums[mid]&&nums[mid]!=nums[mid+1]) return nums[mid];
-            else if ((!(mid%2)&&nums[mid-1]==nums[mid])||(mid%2&&nums[mid]==nums[mid+1]))
-                high=mid-1;
-            else low=mid+1;
+            if(mid%2){
+                if(0<mid&&nums[mid-1]==nums[mid]) low=mid+1;
+                else if(mid<n-1&&nums[mid]==nums[mid+1]) high=mid-1;
+                else return nums[mid];
+            }
+            else{
+                if(0<mid&&nums[mid-1]==nums[mid]) high=mid-1;
+                else if(mid<n-1&&nums[mid]==nums[mid+1]) low=mid+1;
+                else return nums[mid];
+            }
         }
         return -1;
     }
