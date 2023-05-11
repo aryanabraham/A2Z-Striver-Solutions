ceae45570f319fcf857a9c06a0a3ce30a79d9785
Aryan Abraham <abraham.aryan01@gmail.com>
2023-05-11 11:02:05 +0530

    Update Second Largest Element in an Array without sorting.cpp

--- 3/3.1/Second Largest Element in an Array without sorting.cpp
+++ 3/3.1/Second Largest Element in an Array without sorting.cpp
@@ -4,11 +4,16 @@ public:
 	// largest elements
 	int print2largest(int arr[], int n) {
 	    // code here
-	    int lrg=*max_element(arr, arr+n);
+	    int lrg=arr[0];
 	    int slrg=-1;
 	    
 	    for(int i=0;i<n;i++){
-	        if(arr[i]!=lrg&&arr[i]>slrg) slrg=arr[i];
+	        if(arr[i]>lrg){
+	            slrg=lrg;
+	            lrg=arr[i];
+	        }
+	        else if(arr[i]!=lrg&&arr[i]>slrg) 
+	            slrg=arr[i];
 	    }
 	    return slrg;
 	}
