49ccd7e5da47eb32275a44d10b218ae046ecad3c
Aryan Abraham <abraham.aryan01@gmail.com>
2023-05-14 00:45:42 +0530

    Update Merge Sort.cpp

--- 2/2.2/Merge Sort.cpp
+++ 2/2.2/Merge Sort.cpp
@@ -9,8 +9,8 @@ void merge(int l, int mid, int r, vector<int>& nums){
     vector<int> a, b;
     int n1=mid-l+1, n2=r-mid;
     
-    for(int i=0;i<n1;i++) a.push_back(nums[l+i]);
-    for(int j=0;j<n2;j++) b.push_back(nums[mid+j+1]);
+    for(i=0;i<n1;i++) { a.push_back(nums[l+i]); }
+    for(j=0;j<n2;j++) { b.push_back(nums[mid+j+1]); }
     
     i=j=0;
     k=l;
@@ -19,8 +19,8 @@ void merge(int l, int mid, int r, vector<int>& nums){
         if(a[i]<b[j]) nums[k++]=a[i++];
         else nums[k++]=b[j++];
     }
-    while(i<n1) {nums[k++]=a[i++];}
-    while(j<n2) {nums[k++]=b[j++];}
+    while(i<n1) { nums[k++]=a[i++]; }
+    while(j<n2) { nums[k++]=b[j++]; }
 }
 
 void mergeSort(int l, int r, vector<int>& nums){
@@ -34,7 +34,7 @@ void mergeSort(int l, int r, vector<int>& nums){
 }
 
 int main(){
-    vector<int> nums={30, 10, 50, 20, 40};
+    vector<int> nums={20, 50, 10, 30, 40};
     
     mergeSort(0, nums.size()-1, nums);
     for(auto &it: nums)
