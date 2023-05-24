5603430cdfa18cf505c07057a9cea032299b878d
Aryan Abraham <abraham.aryan01@gmail.com>
2023-05-24 22:36:12 +0530

    Create Implement Stack using Arrays.cpp

---
+++ 9/9.1/Implement Stack using Arrays.cpp
@@ -0,0 +1,25 @@
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+void MyStack :: push(int x)
+{
+    // Your Code
+    top++;
+    arr[top]=x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code
+    return top==-1?-1:arr[top--];
+}
